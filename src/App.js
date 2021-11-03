d29b1cb752a7319e5e37d78fdd67a5e9342c0dd0
Alex Fourmy <alexfourmy@gmail.com>
2021-11-03 18:44:46 +0100

    Added a tasks array and created the delete task feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-function App() {
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Dentist Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'work Out',
+      day: 'Feb 6th at 12:30pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 7th at 5:30pm',
+      reminder: true
+    }
+  ])
+
+  // Delete task
+  const deleteTask = id => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
   return (
     <div className="container">
-      <h1>Hello from React</h1>
+      <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   )
 }
