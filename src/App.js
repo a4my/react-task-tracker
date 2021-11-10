e30b472948c35c03da8bebd68cc211f16594b6a6
Alex Fourmy <alexfourmy@gmail.com>
2021-11-10 15:23:31 +0100

    Refactored the code for deploymnent test with Netlify and Heroku

--- src/App.js
+++ src/App.js
@@ -1,3 +1,74 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Workout',
+      day: 'Feb 6th at 12:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Workout',
+      day: 'Feb 6th at 12:30pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 7th at 5:30pm',
+      reminder: false
+    }
+  ])
+
+  // Add task
+  const addTask = task => {
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+  // Delete task
+  const deleteTask = id => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
+  // Toggle reminder
+  const toggleReminder = id => {
+    setTasks(
+      tasks.map(task =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
+  }
+
+  return (
+    <div className="container">
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Tasks To Show'
+      )}
+    </div>
+  )
+}
+
+export default App
+
+/*
+
+
+
+
 import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
@@ -97,3 +168,7 @@ const App = () => {
 }
 
 export default App
+
+
+
+*/
