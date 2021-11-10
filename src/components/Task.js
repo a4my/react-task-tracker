b4756ffbd6086647399a86067d2668e09000446e
Alex Fourmy <alexfourmy@gmail.com>
2021-11-10 15:24:11 +0100

    Refactored the code for deploymnent test with Netlify and Heroku

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,6 +7,37 @@ const Task = ({ task, onDelete, onToggle }) => {
       onDoubleClick={() => onToggle(task.id)}
     >
       <h3>
+        {task.text}
+        <FaTimes
+          style={{ color: 'red', cursor: 'pointer' }}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  )
+}
+
+export default Task
+
+/*
+
+
+
+
+
+
+
+
+import { FaTimes } from 'react-icons/fa'
+
+const Task = ({ task, onDelete, onToggle }) => {
+  return (
+    <div
+      className={`task ${task.reminder ? 'reminder' : ''}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
         {task.text}{' '}
         <FaTimes
           style={{ color: 'red', cursor: 'pointer' }}
@@ -19,3 +50,6 @@ const Task = ({ task, onDelete, onToggle }) => {
 }
 
 export default Task
+
+
+*/
