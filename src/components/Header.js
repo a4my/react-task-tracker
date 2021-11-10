324a4ae1ec2b15523dd4fcd9a0b8a965a7b9d5a7
Alex Fourmy <alexfourmy@gmail.com>
2021-11-10 15:23:50 +0100

    Refactored the code for deploymnent test with Netlify and Heroku

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,36 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? 'red' : 'green'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd}
+      />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
+export default Header
+
+/*
+
+
+
+
+
+
 import Button from './Button'
 
 const Header = ({ title, onAdd, showAdd }) => {
@@ -18,3 +51,5 @@ Header.defaultProps = {
 }
 
 export default Header
+
+*/
